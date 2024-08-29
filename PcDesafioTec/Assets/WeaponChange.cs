7295fd5e94d59a5a02e24a81f0e57cebed9031c7
IsaqueRaFiPetri <irfpetri@gmail.com>
2024-08-29 14:38:23 -0300

    .

--- PcDesafioTec/Assets/WeaponChange.cs
+++ PcDesafioTec/Assets/WeaponChange.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class WeaponChange : MonoBehaviour
 {
-    public GameObject slot1, slot2;
+    public GameObject slot1, slot2, slot3;
 
     void Update()
     {
@@ -17,18 +17,32 @@ public class WeaponChange : MonoBehaviour
         {
             Equip2();
         }
+
+        if (Input.GetKeyDown("3"))
+        {
+            Equip3();
+        }
     }
 
     void Equip1()
     {
         slot1.SetActive(true);
         slot2.SetActive(false);
+        slot3.SetActive(false);
     }
 
     void Equip2()
     {
         slot1.SetActive(false);
         slot2.SetActive(true);
+        slot3.SetActive(false);
+    }
+
+    void Equip3()
+    {
+        slot1.SetActive(false);
+        slot2.SetActive(false);
+        slot3.SetActive(true);
     }
 }
 //https://www.youtube.com/watch?v=RyzFwix15Dg
